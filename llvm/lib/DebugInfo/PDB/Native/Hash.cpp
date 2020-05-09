1db642ab7db7ae4a844a44dbeb362d80a4a4f0f2
Leonardo Santagada <santagada@gmail.com>
2020-05-09 19:04:53 +0200

    Use faster crc32

--- llvm/lib/DebugInfo/PDB/Native/Hash.cpp
+++ llvm/lib/DebugInfo/PDB/Native/Hash.cpp
@@ -9,7 +9,7 @@
 #include "llvm/DebugInfo/PDB/Native/Hash.h"
 #include "llvm/ADT/ArrayRef.h"
 #include "llvm/Support/Endian.h"
-#include "llvm/Support/JamCRC.h"
+#include "llvm/Support/Crc32.h"
 #include <cstdint>
 
 using namespace llvm;
@@ -78,8 +78,5 @@ uint32_t pdb::hashStringV2(StringRef Str) {
 
 // Corresponds to `SigForPbCb` in langapi/shared/crc32.h.
 uint32_t pdb::hashBufferV8(ArrayRef<uint8_t> Buf) {
-  JamCRC JC(/*Init=*/0U);
-  JC.update(makeArrayRef<char>(reinterpret_cast<const char *>(Buf.data()),
-                               Buf.size()));
-  return JC.getCRC();
+  return ~crc32_16bytes_prefetch(Buf.data(), Buf.size(), 0xFFFFFFFF);
 }
